9382623efa1f7d4465635854789482f0b31cb2a9
Roman Svitukha <rsvitukha@FGS.LOCAL>
2017-10-05 16:13:53 -0500

    Add project files.

---
+++ NewFulfillmentClientSetup/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace NewFulfillmentClientSetup
+{
+    partial class FGS_iis_main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Main_text_box = new System.Windows.Forms.TextBox();
+            this.Save_button = new System.Windows.Forms.Button();
+            this.Cancel_button = new System.Windows.Forms.Button();
+            this.Path_label = new System.Windows.Forms.Label();
+            this.Host_label = new System.Windows.Forms.Label();
+            this.Message_label = new System.Windows.Forms.Label();
+            this.main_box_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Main_text_box
+            // 
+            this.Main_text_box.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Main_text_box.Location = new System.Drawing.Point(62, 34);
+            this.Main_text_box.MaxLength = 16;
+            this.Main_text_box.Name = "Main_text_box";
+            this.Main_text_box.Size = new System.Drawing.Size(496, 30);
+            this.Main_text_box.TabIndex = 0;
+            this.Main_text_box.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // Save_button
+            // 
+            this.Save_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Save_button.Location = new System.Drawing.Point(62, 179);
+            this.Save_button.Name = "Save_button";
+            this.Save_button.Size = new System.Drawing.Size(85, 33);
+            this.Save_button.TabIndex = 1;
+            this.Save_button.Text = "Save";
+            this.Save_button.UseVisualStyleBackColor = true;
+            this.Save_button.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Cancel_button
+            // 
+            this.Cancel_button.BackColor = System.Drawing.SystemColors.Control;
+            this.Cancel_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cancel_button.Location = new System.Drawing.Point(473, 179);
+            this.Cancel_button.Name = "Cancel_button";
+            this.Cancel_button.Size = new System.Drawing.Size(85, 33);
+            this.Cancel_button.TabIndex = 2;
+            this.Cancel_button.Text = "Cancel";
+            this.Cancel_button.UseVisualStyleBackColor = false;
+            this.Cancel_button.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Path_label
+            // 
+            this.Path_label.AutoSize = true;
+            this.Path_label.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Path_label.Location = new System.Drawing.Point(58, 108);
+            this.Path_label.MaximumSize = new System.Drawing.Size(500, 0);
+            this.Path_label.Name = "Path_label";
+            this.Path_label.Size = new System.Drawing.Size(500, 44);
+            this.Path_label.TabIndex = 3;
+            this.Path_label.Text = "Path:C:\\DEV\\TeamProject\\Fulfillment\\NAME\\Development\\Source\\Fulfillment.Web";
+            // 
+            // Host_label
+            // 
+            this.Host_label.AutoSize = true;
+            this.Host_label.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Host_label.ForeColor = System.Drawing.Color.Black;
+            this.Host_label.Location = new System.Drawing.Point(58, 76);
+            this.Host_label.MaximumSize = new System.Drawing.Size(500, 0);
+            this.Host_label.Name = "Host_label";
+            this.Host_label.Size = new System.Drawing.Size(290, 22);
+            this.Host_label.TabIndex = 4;
+            this.Host_label.Text = "Host: NAME.fulfillment.local";
+            // 
+            // Message_label
+            // 
+            this.Message_label.AutoSize = true;
+            this.Message_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Message_label.ForeColor = System.Drawing.Color.ForestGreen;
+            this.Message_label.Location = new System.Drawing.Point(153, 179);
+            this.Message_label.Name = "Message_label";
+            this.Message_label.Size = new System.Drawing.Size(135, 29);
+            this.Message_label.TabIndex = 5;
+            this.Message_label.Text = "SUCCESS";
+            this.Message_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Message_label.Visible = false;
+            // 
+            // main_box_label
+            // 
+            this.main_box_label.AutoSize = true;
+            this.main_box_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.main_box_label.Location = new System.Drawing.Point(179, 9);
+            this.main_box_label.Name = "main_box_label";
+            this.main_box_label.Size = new System.Drawing.Size(238, 18);
+            this.main_box_label.TabIndex = 7;
+            this.main_box_label.Text = "Please enter a name for new client.";
+            // 
+            // FGS_iis_main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 241);
+            this.Controls.Add(this.main_box_label);
+            this.Controls.Add(this.Message_label);
+            this.Controls.Add(this.Host_label);
+            this.Controls.Add(this.Path_label);
+            this.Controls.Add(this.Cancel_button);
+            this.Controls.Add(this.Save_button);
+            this.Controls.Add(this.Main_text_box);
+            this.KeyPreview = true;
+            this.MaximumSize = new System.Drawing.Size(610, 280);
+            this.MinimumSize = new System.Drawing.Size(610, 280);
+            this.Name = "FGS_iis_main";
+            this.Text = "FGS IIS setup";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Main_text_box;
+        private System.Windows.Forms.Button Save_button;
+        private System.Windows.Forms.Button Cancel_button;
+        private System.Windows.Forms.Label Path_label;
+        private System.Windows.Forms.Label Host_label;
+        private System.Windows.Forms.Label Message_label;
+        private System.Windows.Forms.Label main_box_label;
+    }
+}
+
---
+++ NewFulfillmentClientSetup/Form1.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.Web.Administration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
+
+namespace NewFulfillmentClientSetup
+{
+    public partial class FGS_iis_main : Form
+    {
+        public FGS_iis_main()
+        {
+            InitializeComponent();
+            Load += new EventHandler(FGS_iis_main_Load);
+        }
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            MessageBox.Show(e.KeyChar.ToString());
+        }
+        private void FGS_iis_main_Load(object sender, System.EventArgs e)
+        {
+            if (!IsAdministrator())
+            {
+                
+                Message_label.Visible = true;
+                Message_label.Text = "You are not in admin mode".ToUpper();
+                Message_label.ForeColor = System.Drawing.Color.Red;
+                Message_label.Font = new Font("Tahoma", 15, FontStyle.Regular);
+            }
+        }
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            Host_label.Text = "Host: " + Main_text_box.Text.Trim() + ".fulfillment.local";
+            Path_label.Text = "Path:C:\\DEV\\TeamProject\\Fulfillment\\" + Main_text_box.Text.Trim() + "\\Development\\Source\\Fulfillment.Web";
+        }
+        private void TextBox1_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+
+        }
+        
+
+            private void Button1_Click(object sender, EventArgs e)
+        {
+            if (Main_text_box.Text.Length == 0)
+            {
+                Message_label.ForeColor = Color.Red;
+                Message_label.Text = "Text box is empty!";
+                Message_label.Visible = true;
+                return;
+            }
+            else if (!IsAdministrator())
+            {
+                Message_label.Visible = true;
+                Message_label.Text = "App is not in admin mode".ToUpper();
+                Message_label.ForeColor = System.Drawing.Color.Red;
+                Message_label.Font = new Font("Tahoma", 16, FontStyle.Regular);
+                return;
+            }
+            else
+            {
+                var confirmResult = MessageBox.Show("Are you sure you want to proceed with " + Main_text_box.Text + "?",
+                                        "Set Up IIS",
+                                        MessageBoxButtons.YesNo);
+                if (confirmResult == DialogResult.Yes)
+                {
+                    if (setupIIS(Main_text_box.Text))
+                    {
+                        Message_label.ForeColor = Color.ForestGreen;
+                        Message_label.Text = "Success";
+                        Message_label.Visible = true;
+                    }
+                    else
+                    {
+                        Message_label.ForeColor = Color.Red;
+                        Message_label.Text = "Error";
+                        Message_label.Visible = true;
+                    }
+                }
+            }
+        }
+        private bool setupIIS(string text)
+        {
+            try
+            {
+                var host = Main_text_box.Text.Trim() + ".fulfillment.local"; ;
+                var path = @"C:\DEV\TeamProject\Fulfillment\" + Main_text_box.Text.Trim() + @"\Development\Source\Fulfillment.Web";
+                CreateIISSite(host, path);
+                AddHostsEntry(host);
+                return true;
+            }
+            catch (Exception e)
+            {
+                File.WriteAllText("C:\\", e.Message + "exception from setup IIS");
+            }
+            return false;
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            var confirmResult = MessageBox.Show("Are you sure to exit?",
+                                     "Exit!",
+                                     MessageBoxButtons.YesNo);
+            if (confirmResult == DialogResult.Yes) System.Windows.Forms.Application.Exit();
+
+            Message_label.Visible = false;
+        }
+
+        private static void CreateIISSite(string host, string path)
+        {
+            ServerManager serverManager = new ServerManager();
+            var applicationPool = serverManager.ApplicationPools.FirstOrDefault(p => p.Name == host);
+            if (applicationPool == null)
+            {
+                applicationPool = serverManager.ApplicationPools.Add(host);
+                applicationPool.ManagedPipelineMode = ManagedPipelineMode.Integrated;
+                applicationPool.ProcessModel.IdentityType = ProcessModelIdentityType.SpecificUser;
+                applicationPool.ProcessModel.UserName = @"***\*****";
+                applicationPool.ProcessModel.Password = "******";//needs to be updated.
+                applicationPool.ManagedRuntimeVersion = "v4.0";
+            }
+
+            var site = serverManager.Sites.FirstOrDefault(s => s.Name == host);
+            if (site == null)
+            {
+                var bindingInfo = string.Format(@"{0}:{1}:{2}", "*", "80", host);
+                site = serverManager.Sites.Add(host, "http", bindingInfo, path);
+                site.ApplicationDefaults.ApplicationPoolName = applicationPool.Name;
+            }
+            serverManager.CommitChanges();
+        }
+        private static void AddHostsEntry(string host)
+        {
+            var hostsFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), "drivers/etc/hosts");
+            var exists = File.ReadAllLines(hostsFile).Any(x => !x.Trim().StartsWith("#") && x.ToLower().Contains(host.ToLower()));
+            if (!exists)
+            {
+                File.AppendAllLines(hostsFile, new[] { string.Format("127.0.0.1		{0}", host) });
+            }
+        }
+        public static bool IsAdministrator()
+        {
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+    }
+}
